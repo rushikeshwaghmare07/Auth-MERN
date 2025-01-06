111415a9e03c50041266cc0680a152c5d78d52ba
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-06 22:59:36 +0530

    Add routes for "sendResetOtp" and "resetPassword"

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -6,6 +6,8 @@ import {
   sendVerifyOtp,
   verifyEmail,
   isAuthenticated,
+  sendResetOtp,
+  resetPassword,
 } from "../controllers/auth.controller.js";
 import userAuth from "../middleware/auth.middleware.js";
 
@@ -17,5 +19,7 @@ router.post("/logout", logoutUser);
 router.post("/send-verify-otp", userAuth, sendVerifyOtp);
 router.post("/verify-account", userAuth, verifyEmail);
 router.post("/is-auth", userAuth, isAuthenticated);
+router.post("/send-reset-otp", sendResetOtp);
+router.post("/reset-password", resetPassword);
 
 export default router;
