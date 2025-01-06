f7b8221dc46bcef2631ac93d53dec619ee5e092d
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-06 22:01:04 +0530

    Add routes for "sendVerifyOtp" and "verifyEmail"

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  sendVerifyOtp,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
+import userAuth from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/send-verify-otp", userAuth, sendVerifyOtp);
+router.post("/verify-account", userAuth, verifyEmail);
 
 export default router;
