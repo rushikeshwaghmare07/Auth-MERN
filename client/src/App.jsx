c42e6249472d8e68dc5ab7811df977dbcf622aa2
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-08 21:10:36 +0530

    Add Routes for Home, Login, Verify-Email and Reset-Password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/email-verify' element={<EmailVerify/>}  />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   )
 }
 
