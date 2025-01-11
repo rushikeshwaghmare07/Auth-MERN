4dea250f79318ea25f4e5ad1ec9ebe19a10a50bf
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-11 19:00:52 +0530

    Add React Toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Routes, Route} from "react-router-dom"
-import Home from './pages/Home'
-import Login from './pages/Login'
-import EmailVerify from './pages/EmailVerify'
-import ResetPassword from './pages/ResetPassword'
-import NotFound from './pages/NotFound'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
+import NotFound from "./pages/NotFound";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/email-verify' element={<EmailVerify/>}  />
-        <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/email-verify" element={<EmailVerify />} />
+          <Route path="/ " element={<ResetPassword />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
