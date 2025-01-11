f8996a2cd1d97ea86c1f17246654fd24e1b0ca8b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-11 19:28:42 +0530

    Add Get Auth State function

--- client/src/components/context/AppContext.jsx
+++ client/src/components/context/AppContext.jsx
@@ -1,22 +1,38 @@
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-export const AppContent = createContext();
+export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserDate] = useState(false);
 
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
+      if (data.success) {
+        isLoggedIn(true);
+        getUserData();
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const getUserData = async () => {
     try {
       const { data } = await axios.get(backendUrl + "/api/user/data");
       data.success ? setUserDate(data.userData) : toast.error(data.message);
     } catch (error) {
-      toast.error(data.message);
+      toast.error(error.message);
     }
   };
+
+  useEffect(() => {
+    getAuthState();
+  }, []);
 
   const value = {
     backendUrl,
@@ -28,6 +44,6 @@ export const AppContextProvider = (props) => {
   };
 
   return (
-    <AppContent.Provider value={value}>{props.children}</AppContent.Provider>
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
 };
