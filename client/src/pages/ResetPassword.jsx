4ec6866e55f1e4fa49fa52004d6e5a3ea16b58fb
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-12 20:01:31 +0530

    Update Reset password "Add Email id form"

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useContext, useRef, useState } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const ResetPassword = () => {
-  return (
-    <div>
-      <h1>Reset Password</h1>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [isEmailSent, setIsEMailSent] = useState("");
 
-export default ResetPassword
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-green-300 via-purple-300 to-purple-500">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt=""
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer "
+      />
+      
+      {/* Enter email id */}
+      {!isEmailSent && (
+        <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+          <h1 className="text-white text-2xl font-semibold text-center mb-4">
+            Reset Password
+          </h1>
+          <p className="text-center mb-6 text-indigo-300">
+            Enter your register email address.
+          </p>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="mail_icon" className="w-3 h-3" />
+            <input
+              type="email"
+              placeholder="Email id"
+              name=""
+              id=""
+              className="bg-transparent outline-none text-white"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <button className="w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-3">
+            Submit
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default ResetPassword;
